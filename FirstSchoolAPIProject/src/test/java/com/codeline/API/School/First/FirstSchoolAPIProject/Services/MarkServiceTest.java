0ea2296edbf5324076ddd4ba1d1c3a3e0262bf72
MarwaAlSinawi <116248656+MarwaAlSinawi@users.noreply.github.com>
2023-03-27 12:22:34 +0400

    Junit test for getbyId for mark

--- FirstSchoolAPIProject/src/test/java/com/codeline/API/School/First/FirstSchoolAPIProject/Services/MarkServiceTest.java
+++ FirstSchoolAPIProject/src/test/java/com/codeline/API/School/First/FirstSchoolAPIProject/Services/MarkServiceTest.java
@@ -1,17 +1,35 @@
 package com.codeline.API.School.First.FirstSchoolAPIProject.Services;
 
+import com.codeline.API.School.First.FirstSchoolAPIProject.Controlles.MarkController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class MarkServiceTest {
-
+    @Autowired
+    MarkController markController;
     @Test
     void getAllMarks() {
     }
 
     @Test
     void getMarkById() {
+        String Mark = markController.getMarkById(1).getGrade();
+        assertEquals("A+",Mark);
+    }
+    void getMarkById1() {
+        String Mark = markController.getMarkById(2).getGrade();
+        assertEquals("A+",Mark);
+    }
+    void getMarkById2() {
+        String Mark = markController.getMarkById(2).getGrade();
+        assertEquals("B",Mark);
+    }
+    void getMarkById3() {
+        String Mark = markController.getMarkById(2).getGrade();
+        assertEquals("C+",Mark);
     }
 
     @Test

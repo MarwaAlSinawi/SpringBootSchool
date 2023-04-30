1cc039082f6e50ab32fc49f4c4dc0c74ee242cf5
MarwaAlSinawi <116248656+MarwaAlSinawi@users.noreply.github.com>
2023-04-30 09:29:23 +0400

    updated q7,q8

--- FirstSchoolAPIProject/src/main/java/com/codeline/API/School/First/FirstSchoolAPIProject/Controlles/ReportController.java
+++ FirstSchoolAPIProject/src/main/java/com/codeline/API/School/First/FirstSchoolAPIProject/Controlles/ReportController.java
@@ -73,4 +73,5 @@ public class ReportController {
     public String generateDistributionOfGradesReport()throws Exception{
         return reportService.generateTheDistributionOfGrades();
     }
+
 }
